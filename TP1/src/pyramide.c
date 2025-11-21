a9f3e7748fe4bcfb3eb35c5eda45b670e55fba44
Stefanotr <180202289+Stefanotr@users.noreply.github.com>
2025-11-21 10:40:20 +0100

    TP1 - Exercice 1.10

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+int main() {
+
+    int n = 5;   // hauteur de la pyramide
+    int i, j;
+
+    for (i = 1; i <= n; i++) {
+
+        // --- 1. Affichage des espaces pour centrer ---
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+
+        // --- 2. Affichage des nombres croissants ---
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        // --- 3. Affichage des nombres décroissants ---
+        for (j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        // Fin de ligne
+        printf("\n");
+    }
+
+    printf("Generation de la pyramide terminee.\n");
+
+    return 0;
+}
